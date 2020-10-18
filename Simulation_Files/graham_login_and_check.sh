2623195b2823219dfc4dd2a5d53b5b9a41ca656c
Brendon Phillips <b2philli@uwaterloo.ca>
2020-10-17 23:30:59 -0400

    Delete graham_login_and_check.sh

--- Simulation_Files/graham_login_and_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sshpass -p "Brendon1993" ssh -tt -o StrictHostKeyChecking=no b2philli@graham.sharcnet.ca << EOF
-        cd /home/b2philli;
-	# touch logged_in.txt;
-	. release_the_kraken_SHARCNET.sh;
-	exit;
-EOF
-
